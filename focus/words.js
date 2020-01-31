79dab4f47746324ac52546e475bf2ff65adb1749
j0k <yuri@silkmind.com>
2020-01-31 22:24:35 +0300

    update with actions

--- focus/words.js
+++ focus/words.js
@@ -1,9 +1,1 @@
 
-var eWordsWant;
-var eWordsWantText;
-var eWordsWantTextElements = [];
-window.onload = function(){
-  eWordsWant = $("#wordsWant");
-  eWordsWantText = eWordsWant.text();
-  eWordsWantTextElements = $("#wordsWant").text().split(",").map(x => x.trim());
-}
