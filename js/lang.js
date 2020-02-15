5baf8d8e36089ab64e36dcbe587cfcf96ad13853
NJ <xakonde@gmail.com>
2020-02-16 00:32:18 +0300

    update languages and images

---
+++ js/lang.js
@@ -0,0 +1,26 @@
+function setClassStyleProperty(cls, prop, value){
+  items = document.getElementsByClassName(cls);
+  for (var i=0;i<items.length;i++){
+    items[i].style[prop] = value;
+  }
+}
+
+function handleLanuageChange(cb) {
+  if (cb.checked) {
+    // check en
+    setClassStyleProperty('ru', 'display', 'none')
+    setClassStyleProperty('en', 'display', 'block')
+  } else {
+    // check ru
+    setClassStyleProperty('en', 'display', 'none')
+    setClassStyleProperty('ru', 'display', 'block')
+  }
+  // document.getElementById("debug").innerHTML = "whatever "+ cb.checked;
+}
+
+
+function ready() {
+   handleLanuageChange(document.getElementById('language'));
+ }
+
+document.addEventListener("DOMContentLoaded", ready);
