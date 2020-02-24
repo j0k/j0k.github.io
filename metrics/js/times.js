7629cab7304a5e2e80212eca3e0fd45744961761
j0k <yuri@silkmind.com>
2020-02-24 17:11:54 +0300

    metrics update

--- metrics/js/times.js
+++ metrics/js/times.js
@@ -98,4 +98,185 @@ svg.append('rect')
 
 var tooltipGroup19 = svg
       .append('g')
-      .attr('class', 'tooltip19')
+      .attr('class', 'tooltip19');
+
+
+
+// -----
+
+svg = d3.select("#sleep_20022020")
+           .append('svg')
+           .attrs({ width: 750, height: 50 });
+
+
+pos19 = $('#sleep_20022020').offset();
+// https://www.a11ywithlindsey.com/blog/accessibility-d3-donut-charts
+
+
+var tTime20 = "-";
+svg.append('rect')
+  .attrs({ x: w*6, y: 0, width: w*6 + w*24, height: 20, fill: 'grey' })
+  .style("stroke", bordercolor)
+  .style("stroke-width", border);
+svg.append('rect')
+  .attrs({ x: 0, y: 0, width: w*6, height: 20, fill: 'white' })
+  .style("stroke", bordercolor)
+  .style("stroke-width", border);
+svg.append('rect')
+   .attrs({ x: w*6 + 2 *w, y: 0,
+            width: 1 *w, height: 20,
+            fill: 'yellow'})
+    //'aria-labelledby' :'unique-id-123'
+   .style("stroke", bordercolor)
+   .style("stroke-width", border)
+   .on('mousemove', () => {
+        const {clientX, clientY} = d3.event
+        d3.select('.tooltip20')
+          .attr('transform', `translate(${clientX - pos19.left+15} ${clientY - pos19.top + 15})`)
+      })
+      .on('mouseenter', d => {
+        d3.select('.tooltip20').append('text')
+          .text(tTime20)
+      })
+      .on('mouseleave', () => d3.select('.tooltip20 text').remove());
+
+// 12:40 - 3:20
+svg.append('rect')
+   .attrs({ x: 6*w + 5.5*w, y: 0,
+            width: 6 *w, height: 20,
+            fill: 'green'})
+    //'aria-labelledby' :'unique-id-123'
+   .style("stroke", bordercolor)
+   .style("stroke-width", border)
+
+  svg.append("text")
+           .attrs({x: w*6 - 2.5 *w, y: 40})
+           .attr("dy", "0em")
+           .text(tTime20);
+
+var tooltipGroup20 = svg
+      .append('g')
+      .attr('class', 'tooltip20');
+
+
+
+
+/// ---
+svg = d3.select("#sleep_24022020")
+           .append('svg')
+           .attrs({ width: 750, height: 50 });
+
+
+var tTime24 = "3:10 am - 9:30 am; dt=6h";
+svg.append('rect')
+  .attrs({ x: w*6, y: 0, width: w*6 + w*24, height: 20, fill: 'grey' })
+  .style("stroke", bordercolor)
+  .style("stroke-width", border);
+svg.append('rect')
+  .attrs({ x: 0, y: 0, width: w*6, height: 20, fill: 'white' })
+  .style("stroke", bordercolor)
+  .style("stroke-width", border);
+svg.append('rect')
+   .attrs({ x: w*6 + 3.1 *w, y: 0,
+            width: 6 *w, height: 20,
+            fill: 'green'})
+    //'aria-labelledby' :'unique-id-123'
+   .style("stroke", bordercolor)
+   .style("stroke-width", border)
+
+// 12:40 - 3:20
+
+svg.append("text")
+           .attrs({x: w*6 + 3.1 *w, y: 40})
+           .attr("dy", "0em")
+           .text(tTime24);
+
+/// ---
+svg = d3.select("#sleep_23022020")
+          .append('svg')
+          .attrs({ width: 750, height: 50 });
+
+
+var tTime23 = "3:30 am - 13:30 pm; dt=10h";
+svg.append('rect')
+ .attrs({ x: w*6, y: 0, width: w*6 + w*24, height: 20, fill: 'grey' })
+ .style("stroke", bordercolor)
+ .style("stroke-width", border);
+svg.append('rect')
+ .attrs({ x: 0, y: 0, width: w*6, height: 20, fill: 'white' })
+ .style("stroke", bordercolor)
+ .style("stroke-width", border);
+svg.append('rect')
+  .attrs({ x: w*6 + 3.50 *w, y: 0,
+           width: 10 *w, height: 20,
+           fill: 'green'})
+   //'aria-labelledby' :'unique-id-123'
+  .style("stroke", bordercolor)
+  .style("stroke-width", border)
+
+// 12:40 - 3:20
+
+svg.append("text")
+          .attrs({x: w*6 + 3.1 *w, y: 40})
+          .attr("dy", "0em")
+          .text(tTime23);
+
+/// ---
+svg = d3.select("#sleep_22022020")
+          .append('svg')
+          .attrs({ width: 750, height: 50 });
+
+
+var tTime22 = "5:30 am - 14:30 pm; dt=9h";
+svg.append('rect')
+ .attrs({ x: w*6, y: 0, width: w*6 + w*24, height: 20, fill: 'grey' })
+ .style("stroke", bordercolor)
+ .style("stroke-width", border);
+svg.append('rect')
+ .attrs({ x: 0, y: 0, width: w*6, height: 20, fill: 'white' })
+ .style("stroke", bordercolor)
+ .style("stroke-width", border);
+svg.append('rect')
+  .attrs({ x: w*6 + 5.50 *w, y: 0,
+           width: 9 *w, height: 20,
+           fill: 'green'})
+   //'aria-labelledby' :'unique-id-123'
+  .style("stroke", bordercolor)
+  .style("stroke-width", border)
+
+// 12:40 - 3:20
+
+svg.append("text")
+          .attrs({x: w*6 + 3.1 *w, y: 40})
+          .attr("dy", "0em")
+          .text(tTime22);
+
+  /// ---
+  svg = d3.select("#sleep_21022020")
+            .append('svg')
+            .attrs({ width: 750, height: 50 });
+
+
+  var tTime21 = "3:00 am - 9:00 pm; dt=6h";
+  svg.append('rect')
+   .attrs({ x: w*6, y: 0, width: w*6 + w*24, height: 20, fill: 'grey' })
+   .style("stroke", bordercolor)
+   .style("stroke-width", border);
+  svg.append('rect')
+   .attrs({ x: 0, y: 0, width: w*6, height: 20, fill: 'white' })
+   .style("stroke", bordercolor)
+   .style("stroke-width", border);
+  svg.append('rect')
+    .attrs({ x: w*6 + 3.0 *w, y: 0,
+             width: 6 *w, height: 20,
+             fill: 'green'})
+     //'aria-labelledby' :'unique-id-123'
+    .style("stroke", bordercolor)
+    .style("stroke-width", border)
+
+  // 12:40 - 3:20
+
+  svg.append("text")
+            .attrs({x: w*6 + 3.1 *w, y: 40})
+            .attr("dy", "0em")
+            .text(tTime21);
